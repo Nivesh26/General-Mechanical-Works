347947b29f6883dcdab37e5ed32b1bbedda37115
Nivesh26 <nivesh.shrestha.94@gmail.com>
2026-02-01 17:47:05 +0545

    Add admin and user login/signup components; update routing in App component to include new paths for admin and user authentication.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,6 +5,10 @@ import Aboutus from './UserPages/Aboutus'
 import Service from './UserPages/Service'
 import Products from './UserPages/Products'
 import Contactus from './UserPages/Contactus'
+import Adminlogin from './Logins-Signup/Adminlogin'
+import Adminsignup from './Logins-Signup/Adminsignup'
+import Userlogin from './Logins-Signup/Userlogin'
+import Usersignup from './Logins-Signup/Usersignup'
 
 function App() {
   return (
@@ -16,6 +20,15 @@ function App() {
     <Route path="/services" element={<Service />} />
     <Route path="/products" element={<Products />} />
     <Route path="/contactus" element={<Contactus />} />
+
+    {/* Admin Login and Signup */}
+    <Route path="/adminlogin" element={<Adminlogin />} />
+    <Route path="/adminsignup" element={<Adminsignup />} />
+
+    {/* User Login and Signup */}
+    <Route path="/userlogin" element={<Userlogin />} />
+    <Route path="/usersignup" element={<Usersignup />} />
+
     </Routes>
     </BrowserRouter>
   )
--- Frontend/src/UserPages/Home.tsx
+++ Frontend/src/UserPages/Home.tsx
@@ -4,6 +4,7 @@ import Footer from '../UserComponent/Footer'
 import Copyright from '../UserComponent/Copyright'
 import Hero from '../UserComponent/Hero'
 import Brandweserve from '../UserComponent/Brandweserve'
+import Offer from '../UserComponent/Offer'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <Header />
         <Hero />
         <Brandweserve />                                  
+        <Offer />
         <Footer />
         <Copyright />
     </div>
