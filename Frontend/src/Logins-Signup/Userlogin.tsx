24bce468f0cebb9feb3b9bccd681491779894056
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-02-05 23:51:40 +0545

    Integrate Header, Footer, and Copyright components into Userlogin for improved layout and user experience.

--- Frontend/src/Logins-Signup/Userlogin.tsx
+++ Frontend/src/Logins-Signup/Userlogin.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../UserComponent/Header'
+import Footer from '../UserComponent/Footer'
+import Copyright from '../UserComponent/Copyright'
 
 const Userlogin = () => {
   return (
-    <div>Userlogin</div>
+    <div>
+      <Header />
+      <Footer />
+      <Copyright />
+    </div>
   )
 }
 
